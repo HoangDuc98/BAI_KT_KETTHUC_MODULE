49335f0f3ce54d3524109dc191949294334ff2fe
Hoang Duc <duccoc98@gmail.com>
2020-10-07 11:32:22 +0700

    1

--- js4/Circle.js
+++ js4/Circle.js
@@ -5,4 +5,14 @@ class Circle {
         this.radius = radius;
         this.color = color;
     }
+    render(drawCircle){
+        let c = document.getElementById("myCanvas");
+        let ctx = c.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(10,10,100,0,2*Math.PI);
+        ctx.stroke();
+
+    }
 }
+let Circle1 = new Circle(10,10,100,0,);
+Circle1.render();
